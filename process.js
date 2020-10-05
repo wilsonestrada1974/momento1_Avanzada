60dd8ef843e1af949a45ffb4e310f6689e304ccb
wilson Estrada <wilson.estrada@gmail.com>
2020-10-04 23:02:45 -0500

    trabajo realizado satisfactoriamente

--- process.js
+++ process.js
@@ -1,22 +1,36 @@
-function procesar(){
-    let service =document.getElementById('service').value;
-    let cant = parseInt(document.getElementById('cant').value);
-    let value = parseInt (document.getElementById('value').value);
-    let acumm;
-    let sum;
-    let subt=0;
-    let prom=0;
+'use strict';
 
-    do{
-    
-        if(cant>0 && value>0){
-        
-        subt=cant*value;
-        sum=sum+subt;
-        }
-        acumm++;
-        prom=sum/acumm;
-    }while(acumm=0);
 
-    return(sum, acumm, prom, subt);
+var acum = 0;
+var sum = 0;
+var prom = 0;
+
+var result = document.querySelector("#resultados");
+result.style.display = "none";
+
+
+
+function prosesar() {
+
+
+    let service = document.getElementById("sevice").value;
+    let cant = parseInt(document.getElementById("cant").value);
+    let value = parseInt(document.getElementById("value").value);
+    var p_cant_ser = document.querySelector("#p_cant_ser span");
+    var p_suma_ser = document.querySelector("#p_suma_ser span");
+    var p_prom_ser = document.querySelector("#p_prom_ser span");
+
+    result.style.display = "block";
+
+    acum++;
+    sum = sum + value;
+    prom = sum / acum;
+
+    p_cant_ser.innerHTML = acum;
+    p_suma_ser.innerHTML = sum;
+    p_prom_ser.innerHTML = prom;
+
+
+
+
 }
